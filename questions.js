e9f23a78633e36315ca12932aa0b605978641b39
Piyush Barve <barvepiyush1000@gmail.com>
2020-08-22 01:02:36 +0530

    session storage func added

--- questions.js
+++ questions.js
@@ -3,35 +3,47 @@ function myfunction(){
     var val2= document.getElementById('val2');
     var val3= document.getElementById('val3');
     var val4= document.getElementById('val4');
-    var points=0;
+    let points=sessionStorage.getItem('marks');
+;
+    
     if (val1.checked==true){
         alert('Wrong Answer ');
         points=points-1;
         document.getElementById("btn1").disabled = true;
-        document.getElementById('point1').innerHTML= 'Points Awarded: ' + points;
+        document.getElementById('point2').innerHTML= 'Total Points: ' + points;
+        document.getElementById('point1').innerHTML= 'Points Awarded: -1 ' ;
+        
     }
     else if (val2.checked==true){
         alert('Right Answer');
-        points=points+4;
+        points=parseInt(points) + 4;
         document.getElementById("btn1").disabled = true;
-        document.getElementById('point1').innerHTML= 'Points Awarded: '+points;
+        document.getElementById('point2').innerHTML= 'Total Points: '+points;
+        document.getElementById('point1').innerHTML= 'Points Awarded: +4 '; 
     }
     else if (val3.checked==true){
         alert('Wrong Answer ');
         points=points-1;
         document.getElementById("btn1").disabled = true;
-        document.getElementById('point1').innerHTML= 'Points Awarded: '+points;
+        document.getElementById('point2').innerHTML= 'Total Points: '+points;
+        document.getElementById('point1').innerHTML= 'Points Awarded: -1 ' ;
     }
     else if (val4.checked==true){
         alert('Wrong Answer ');
         points=points-1;
         document.getElementById("btn1").disabled = true;
-        document.getElementById('point1').innerHTML= 'Points Awarded: '+points;
+        document.getElementById('point2').innerHTML= 'Total Points: '+points;
+        document.getElementById('point1').innerHTML= 'Points Awarded: -1 ' ;
     }
     else if (val1.checked==false && val2.checked==false && val3.checked==false && val4.checked==false){
         alert('No option is selected');
     }
+    
+    
+    sessionStorage.setItem('marks',points);
     }
+
+    
     
         
     
