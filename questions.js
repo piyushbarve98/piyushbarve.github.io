e6333ad89bfe577b60d2bf4342d289977fcf75fb
Piyush Barve <barvepiyush1000@gmail.com>
2020-08-26 14:25:39 +0530

    updated

--- questions.js
+++ questions.js
@@ -4,7 +4,7 @@ function myfunction(){
     var val3= document.getElementById('val3');
     var val4= document.getElementById('val4');
     let points=sessionStorage.getItem('marks');
-    Navigator.vibrate(200);
+    window.navigator.vibrate(300);
     sound=document.getElementById('myAudio');
     sound.play();
     
