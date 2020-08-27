9383ea267a082c031447e2b38bb690785ed844d5
Piyush Barve <barvepiyush1000@gmail.com>
2020-08-28 00:24:49 +0530

    updated

--- questions.js
+++ questions.js
@@ -5,12 +5,9 @@ function myfunction(){
     var val4= document.getElementById('val4');
     let points=sessionStorage.getItem('marks');
     window.navigator.vibrate(200);
-    sound=document.getElementById('right');
-    sound1.play();
-    sound1=document.getElementById('wrong');
-    sound.play();
-    sound2=document.getElementById('noAnswer');
-    sound2.play();
+    
+    
+    
     
     if (val1.checked==true){
         alert('Wrong Answer ');
@@ -18,6 +15,8 @@ function myfunction(){
         document.getElementById("btn1").disabled = true;
         document.getElementById('point2').innerHTML= 'Total Points: ' + points;
         document.getElementById('point1').innerHTML= 'Points Awarded: -1 ' ;
+        sound1=document.getElementById('wrong');
+        sound.play();
         
     }
     else if (val2.checked==true){
@@ -26,6 +25,8 @@ function myfunction(){
         document.getElementById("btn1").disabled = true;
         document.getElementById('point2').innerHTML= 'Total Points: '+points;
         document.getElementById('point1').innerHTML= 'Points Awarded: +4 '; 
+        sound=document.getElementById('right');
+        sound1.play();
     }
     else if (val3.checked==true){
         alert('Wrong Answer ');
@@ -33,6 +34,8 @@ function myfunction(){
         document.getElementById("btn1").disabled = true;
         document.getElementById('point2').innerHTML= 'Total Points: '+points;
         document.getElementById('point1').innerHTML= 'Points Awarded: -1 ' ;
+        sound1=document.getElementById('wrong');
+        sound.play();
     }
     else if (val4.checked==true){
         alert('Wrong Answer ');
@@ -40,9 +43,13 @@ function myfunction(){
         document.getElementById("btn1").disabled = true;
         document.getElementById('point2').innerHTML= 'Total Points: '+points;
         document.getElementById('point1').innerHTML= 'Points Awarded: -1 ' ;
+        sound1=document.getElementById('wrong');
+        sound.play();
     }
     else if (val1.checked==false && val2.checked==false && val3.checked==false && val4.checked==false){
         alert('No option is selected');
+        sound2=document.getElementById('noAnswer');
+        sound2.play();
     }
     
     
