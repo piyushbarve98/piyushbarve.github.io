d1717ca7931201cdf17bf61fac9ce75937bb9c95
Piyush Barve <barvepiyush1000@gmail.com>
2020-08-28 18:18:21 +0530

    updated

--- questions.js
+++ questions.js
@@ -4,52 +4,58 @@ function myfunction(){
     var val3= document.getElementById('val3');
     var val4= document.getElementById('val4');
     let points=sessionStorage.getItem('marks');
-    window.navigator.vibrate(200);
-    
-    
     
     
     if (val1.checked==true){
+        window.navigator.vibrate(200);
+        sound1=document.getElementById('wrong');
+        sound1.play();
         alert('Wrong Answer ');
         points=points-1;
         document.getElementById("btn1").disabled = true;
         document.getElementById('point2').innerHTML= 'Total Points: ' + points;
         document.getElementById('point1').innerHTML= 'Points Awarded: -1 ' ;
-        sound1=document.getElementById('wrong');
-        sound1.play();
+        
         
     }
     else if (val2.checked==true){
+        sound=document.getElementById('right');
+        sound.play();
         alert('Right Answer');
         points=parseInt(points) + 4;
         document.getElementById("btn1").disabled = true;
         document.getElementById('point2').innerHTML= 'Total Points: '+points;
         document.getElementById('point1').innerHTML= 'Points Awarded: +4 '; 
-        sound=document.getElementById('right');
-        sound.play();
+        
     }
     else if (val3.checked==true){
+        window.navigator.vibrate(200);
+        sound1=document.getElementById('wrong');
+        sound1.play();
         alert('Wrong Answer ');
         points=points-1;
         document.getElementById("btn1").disabled = true;
         document.getElementById('point2').innerHTML= 'Total Points: '+points;
         document.getElementById('point1').innerHTML= 'Points Awarded: -1 ' ;
-        sound1=document.getElementById('wrong');
-        sound1.play();
+        
     }
     else if (val4.checked==true){
+        window.navigator.vibrate(200);
+        sound1=document.getElementById('wrong');
+        sound1.play();
         alert('Wrong Answer ');
         points=points-1;
         document.getElementById("btn1").disabled = true;
         document.getElementById('point2').innerHTML= 'Total Points: '+points;
         document.getElementById('point1').innerHTML= 'Points Awarded: -1 ' ;
-        sound1=document.getElementById('wrong');
-        sound1.play();
+        
     }
     else if (val1.checked==false && val2.checked==false && val3.checked==false && val4.checked==false){
-        alert('No option is selected');
+        window.navigator.vibrate(200);
         sound2=document.getElementById('noAnswer');
         sound2.play();
+        alert('No option is selected');
+        
     }
     
     
